0be8fc9379915616a7f24244e40c0e5ab9d3f066
Keeshigan Pirabaharan <kpirabaharan3@gmail.com>
2023-08-20 18:07:41 -0400

    Created Gallery Component
    Signed-off-by: Keeshigan Pirabaharan <kpirabaharan3@gmail.com>

--- app/product/[productId]/page.tsx
+++ app/product/[productId]/page.tsx
@@ -1,11 +1,15 @@
 import getProduct from '@/actions/getProduct';
 import getProducts from '@/actions/getProducts';
-import ProductList from '@/components/product/ProductList';
+
 import Container from '@/components/ui/container';
+import Gallery from '@/components/gallery/Gallery';
+import ProductList from '@/components/product/ProductList';
 
 interface ProductPageProps {
   params: { productId: string };
 }
+
+export const revalidate = 0;
 
 const ProductPage = async ({ params }: ProductPageProps) => {
   const product = await getProduct(params.productId);
@@ -18,8 +22,7 @@ const ProductPage = async ({ params }: ProductPageProps) => {
       <Container>
         <div className='px-4 py-10 sm:px-6 lg:px-8'>
           <div className='lg:grid lg:grid-cols-2 lg:items-start lg:gap-x-8'>
-            {/* Gallery */}
-            <div>Gallery</div>
+            <Gallery images={product.images} />
             <div className='mt-10 px-4 sm:mt-16 sm:px-0 lg:mt-0'>
               {/* Info */}
               Info
---
+++ components/gallery/Gallery.tsx
@@ -0,0 +1,51 @@
+'use client';
+
+import Image from 'next/image';
+
+import { Image as ImageType } from '@/types';
+
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
+import { useState } from 'react';
+
+interface GalleryProps {
+  images: ImageType[];
+}
+
+const Gallery = ({ images }: GalleryProps) => {
+  const [active, setActive] = useState<string>(images[0].id);
+
+  return (
+    <Tabs defaultValue={images[0].id} className='h-[400px] space-y-2 w-full'>
+      <div className='h-[300px] relative bg-gray-200 rounded-md'>
+        {images.map((image) => (
+          <TabsContent key={image.id} value={image.id}>
+            <Image
+              src={image.url}
+              className='object-contain'
+              alt='image'
+              fill
+            />
+          </TabsContent>
+        ))}
+      </div>
+      <TabsList className='h-[100px] gap-x-4'>
+        {images.map((image) => (
+          <div key={image.id} className='w-[100px] h-full relative'>
+            <TabsTrigger value={image.id} onClick={() => setActive(image.id)}>
+              <Image
+                src={image.url}
+                alt='image'
+                className={`object-cover rounded-sm border ${
+                  active === image.id && 'border-black'
+                }`}
+                fill
+              />
+            </TabsTrigger>
+          </div>
+        ))}
+      </TabsList>
+    </Tabs>
+  );
+};
+
+export default Gallery;
